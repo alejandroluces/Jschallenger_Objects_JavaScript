438eb4db4d5ca8be7042f1f02a98806cd433e045
Alejandro Luces González <aluces@polytex.local>
2023-06-19 12:47:49 -0400

    Add_exercise_jschallenger_objects_V6

---
+++ JS_Objects_V6.js
@@ -0,0 +1,24 @@
+// Creación de objetos Javascript uno
+
+// Write a function that takes a string as argument. 
+// Create an object that has a property with key 'key' and a value equal to the string. 
+// Return the object.
+
+
+// Escribe una función que tome una cadena como argumento. 
+// Cree un objeto que tenga una propiedad con la clave 'clave' y un valor igual a la cadena. 
+// Devolver el objeto.
+
+
+const myFunction =(a) =>{
+
+    return { key: a }
+
+}
+console.log(myFunction('a'));
+console.log(myFunction('z'));
+console.log(myFunction('b'));
+
+
+
+
